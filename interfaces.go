a427f0783d92d64baf24bb768aa48c4dc7a4c35e
alifrindo <frindoali@hotmail.com>
2021-11-14 13:31:39 -0500

    Simplify code to view interfaces

--- interfaces.go
+++ interfaces.go
@@ -2,34 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 )
 
 func localAddresses() {
-    ifaces, err := net.Interfaces()
-    if err != nil {
-        fmt.Print(fmt.Errorf("localAddresses: %+v\n", err.Error()))
-        return
-    }
-    for _, i := range ifaces {
-        addrs, err := i.Addrs()
-        if err != nil {
-            fmt.Print(fmt.Errorf("localAddresses: %+v\n", err.Error()))
-            continue
-        }
-        for _, a := range addrs {
-            switch v := a.(type) {
-            case *net.IPAddr:
-                fmt.Printf("%v : %s (%s)\n", i.Name, v, v.IP.DefaultMask())
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		log.Print(fmt.Errorf("localAddresses: %v\n", err.Error()))
+		return
+	}
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			log.Print(fmt.Errorf("localAddresses: %v\n", err.Error()))
+			continue
+		}
+		for _, a := range addrs {
+			log.Printf("%v %v\n", i.Name, a)
+		}
 
-            case *net.IPNet:
-                fmt.Printf("%v : %s [%v/%v]\n", i.Name, v, v.IP, v.Mask)
-            }
-
-        }
-    }
+	}
 }
 
 func main() {
-    localAddresses()
+	localAddresses()
 }
